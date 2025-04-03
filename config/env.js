a6a60fe27919561d459be3fd3339825fad17bc17
omovigho <100778865+omovigho@users.noreply.github.com>
2025-04-03 23:58:39 +0100

    good

---
+++ config/env.js
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+
+import process from "process";
+
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+export const {
+    PORT, NODE_ENV, DB_URI,
+    JWT_SECRET,
+    ARCJET_ENV, ARCJET_KEY,
+} = process.env;
